2f4da7e7904061b09312dcba767c4475a9ce786d
marceloh-dev <marcelohs.dev@gmail.com>
2022-07-02 18:15:34 -0300

    First commit in a spring boot project

---
+++ src/test/java/com/studingspring/studyspring/StudyspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studingspring.studyspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
